70f058e595b14086decdc6fff3a188d583341a05
SILVERTHRONE32 <SILVERTHRONE32@users.noreply.github.com>
2026-05-22 02:36:14 +0000

    Deploying to gh-pages from @ SILVERTHRONE32/silverthrone32.github.io@c0a722623959cfbe6de3bea3bd7d88821c10bf81 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "SILVERTHRONE32/sachinmahesh.github.io",
+    "data-repo": "SILVERTHRONE32/silverthrone32.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
